b57b729c275e5dd89a5ba04681043ad2c26d2741
Alexander Chalk <contact@alexchalk.net>
2019-08-31 08:46:46 -0400

    Add build env draft ideas

--- do/mxnet-shared-build.nix
+++ do/mxnet-shared-build.nix
@@ -1,14 +1,49 @@
+# or { jemalloc }: # check docs for how this syntax works
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
   name = "mxnet-env";
-  nativeBuildInputs = [ pkg-config cmake ];
+  nativeBuildInputs = [ pkgconfig cmake ];
   buildInputs = [
-    # ? binutils
+    # binutils
+    llvm
     graphviz
-    jemalloc
     liblapack
+    # ^- included with openblas
+    jemalloc  # <- need to somehow set paths to both of these as USE_JEMALLOC_PATH/USE_LAPACK_PATH
     openblas
     opencv3
   ];
+  makeFlags = [
+    "-j $(nproc)"
+    "USE_JEMALLOC_PATH=${stdenv.lib.makeLibraryPath [jemalloc]}"
+    "USE_LAPACK_PATH=${stdenv.lib.makeLibraryPath [liblapack]}"
+    "USE_OPENCV=1"
+    "USE_MKLDNN=1"
+    "USE_JEMALLOC=1"
+    "USE_BLAS=openblas"
+  ];
+
+  # makefile = "$HOME/workdir/apache-mxnet/Makefile";
+
+  shellHook = ''
+    JEMALLOC_PATH=${stdenv.lib.makeLibraryPath [jemalloc]}
+    LAPACK_PATH=${stdenv.lib.makeLibraryPath [liblapack]}
+  '';
+  # ];
+# makeLibraryPath [ pkgs.openssl pkgs.zlib ]
+
+# echo "${pkgs.jemalloc}/lib/libjemalloc.so"
+# postFixup = ''
+#     for e in $(cd $out/bin && ls); do
+#       wrapProgram $out/bin/$e \
+#         --prefix PATH : "${gnumake}/bin" \
+#         --prefix LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [ liblapack blas ]}"
+#     done
+#   '';
+  # maybe
+  # cmakeFlags = [
+  #   "-DENABLE_JEMALLOC=ON"
+  # ];
 }
+# $(patchelf --print-rpath jemalloc)
