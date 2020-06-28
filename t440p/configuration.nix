2be92728eced04e109e1a608cbc84aa406bc2f80
Alex Chalk <contact@alexchalk.net>
2020-06-28 19:29:52 -0400

    docs: Improve stateVersion comment

--- t440p/configuration.nix
+++ t440p/configuration.nix
@@ -281,9 +281,7 @@ in
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  # Only update this if you do a completely fresh install.
+  # https://nixos.org/nixos/options.html#system.stateversion
+  system.stateVersion = "19.09";
 }
