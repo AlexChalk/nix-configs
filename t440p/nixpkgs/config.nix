5e089b973755a72a11e162920be45177afd55300
Alex Chalk <contact@alexchalk.net>
2020-11-23 10:39:28 -0500

    chore: Trim unused packages

--- t440p/nixpkgs/config.nix
+++ t440p/nixpkgs/config.nix
@@ -8,12 +8,10 @@
         _1password-gui
         acpi
         appimage-run
-        asciidoctor
         binutils
         bundix
         clojure
         (import <nixos-unstable> {}).clojure-lsp
-        dmidecode
         dropbox-cli
         elmPackages.elm
         elmPackages.elm-format
@@ -25,27 +23,20 @@
         fzf
         gitAndTools.hub
         gnumake
-        handbrake
-        haskellPackages.greenclip
         hledger
         gnome3.adwaita-icon-theme
-        gnome3.gnome-keyring
         grim
         slurp
         wf-recorder
         wl-clipboard
-        gnome3.gnome-maps
-        gnome3.gnome-control-center
         gnome3.simple-scan
         git
         htop
         joker
         jq
-        # julia
         octave
         kitty
         leiningen
-        libappindicator
         lua
       	openjdk
         maven
@@ -69,7 +60,6 @@
         rustup
         (import <nixos-unstable> {}).signal-desktop
         stack
-        subversion
         terraform
         texlive.combined.scheme-medium
         tmux
@@ -80,7 +70,6 @@
         wget
         wine
         winetricks
-        xdg_utils
         yarn # use yarn instead of npm for global installs
         zathura
         (hiPrio gcc)
