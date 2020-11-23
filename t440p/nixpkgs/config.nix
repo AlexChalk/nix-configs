d1e7e1c81beb686679effcb7f702578df730ccd4
Alex Chalk <contact@alexchalk.net>
2020-11-23 10:50:10 -0500

    chore: Sort application list

--- t440p/nixpkgs/config.nix
+++ t440p/nixpkgs/config.nix
@@ -9,59 +9,62 @@
         acpi
         appimage-run
         audacity
-        castget
+        bashmount
         binutils
         bundix
+        castget
         clojure
         (import <nixos-unstable> {}).clojure-lsp
+        dotnet-sdk
+        dotnetPackages.Nuget
         dropbox-cli
         elmPackages.elm
         elmPackages.elm-format
-        elmPackages.elm-test
-        elmPackages.elm-language-server
         elmPackages.elm-json
+        elmPackages.elm-language-server
+        elmPackages.elm-test
         fd
         firefox-wayland
+        fsharp
         fzf
-        gitAndTools.hub
-        gnumake
-        hledger
-        gnome3.adwaita-icon-theme
-        grim
-        slurp
-        wf-recorder
-        wl-clipboard
-        gnome3.simple-scan
+        (hiPrio gcc)
         git
+        gitAndTools.hub
+        gnome3.adwaita-icon-theme
+        gnome3.simple-scan
+        gnumake
+        grim
+        gtkpod
+        hledger
         htop
         joker
         jq
-        octave
         kitty
         leiningen
         lua
-      	openjdk
         maven
         mono
-        dotnet-sdk
-        dotnetPackages.Nuget
-        fsharp
         mpv
+        mtools
         neofetch
         netlify-cli
-        nixops
         nix-index
+        nixops
         nodejs
+        octave
+        openjdk
         pandoc
         python2
-        python3
         python2Packages.pynvim
+        python3
         python3Packages.pynvim
         ripgrep
         ruby
         rustup
         (import <nixos-unstable> {}).signal-desktop
+        slurp
         stack
+        superTuxKart
         terraform
         texlive.combined.scheme-medium
         tmux
@@ -69,18 +72,15 @@
         universal-ctags
         unzip
         viewnior
+        wf-recorder
         wget
         wine
         winetricks
+        wl-clipboard
         yarn # use yarn instead of npm for global installs
         zathura
-        (hiPrio gcc)
         (callPackage (import ./packages/neovim.nix) {})
         (callPackage (import ./packages/emoji-launcher.nix) { inherit pkgs; stdenv = pkgs.stdenv; })
-        superTuxKart
-        gtkpod
-        bashmount
-        mtools
       ];
       pathsToLink = [ "/share" "/bin" ];
     };
