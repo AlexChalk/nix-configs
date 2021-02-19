b131fd6896cfea71325b6266364170b072445742
Alex Chalk <contact@alexchalk.net>
2021-02-19 18:15:02 -0500

    feat: Add first pass at config for mac

---
+++ mac/nixpkgs/config.nix
@@ -0,0 +1,49 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; {
+    adcPackages = pkgs.buildEnv {
+      name = "adc-packages";
+      paths = [
+        anki
+        babashka
+        binutils
+        clojure-lsp
+        curl
+        fd
+        firefox
+        fzf
+        gawk
+        git
+        gitAndTools.gh
+        gitAndTools.hub
+        gnumake
+        gnused
+        heroku
+        htop
+        jq
+        kitty
+        neofetch
+        ngrok
+        nodejs-12_x
+        pandoc
+        python3
+        python3Packages.pynvim
+        pipenv
+        rename
+        ripgrep
+        rlwrap
+        ruby
+        tmux
+        tree
+        universal-ctags
+        watchman
+        wget
+        whois
+        yarn
+        zathura
+        (callPackage (import ./packages/neovim.nix) {})
+      ];
+      pathsToLink = [ "/share" "/bin" ];
+    };
+  };
+}
