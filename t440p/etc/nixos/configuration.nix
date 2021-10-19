6d2f434d875f85d3f31d9a68753112e3129d0e00
Alex Chalk <contact@alexchalk.net>
2021-10-19 16:52:48 -0400

    Use 'proper' gruvbox terminal color scheme

--- t440p/etc/nixos/configuration.nix
+++ t440p/etc/nixos/configuration.nix
@@ -67,8 +67,8 @@ in
     packages = [ pkgs.terminus_font ];
     font = "ter-122n";
     colors = [ 
-      "282828" "fb4934" "b8bb26" "fabd2f" "83a598" "d3869b" "8ec07c" "d5c4a1"
-      "665c54" "fb4934" "b8bb26" "fabd2f" "83a598" "d3869b" "8ec07c" "fbf1c7"
+      "282828" "cc241d" "98971a" "d79921" "458588" "b16286" "689d6a" "a89984"
+      "928374" "fb4934" "b8bb26" "fabd2f" "83a598" "d3869b" "8ec07c" "ebdbb2"
     ];
   };
 
