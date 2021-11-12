a7dd869e8b6d367151814c84e7438c0b8dc71605
Alex Chalk <contact@alexchalk.net>
2021-11-12 15:11:58 -0500

    Read neovim config from .lua not .vim

--- packages/neovim.nix
+++ packages/neovim.nix
@@ -2,7 +2,7 @@
 
 with pkgs; neovim.override {
   configure = {
-    customRC = "execute 'source $HOME/.config/nvim/init.vim'";
+    customRC = "execute 'source $HOME/.config/nvim/init.lua'";
     packages.myVimPackage = with pkgs.vimPlugins; {
       start = [ fzf-vim fzfWrapper ];
       opt = [ ];
