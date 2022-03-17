cad94675025f2bd8ac1abdbd05da5fec5589c495
Alex Chalk <contact@alexchalk.net>
2022-03-17 12:37:36 -0400

    Add various language support (mainly lsp) to mac

--- mac/nixpkgs/config.nix
+++ mac/nixpkgs/config.nix
@@ -1,29 +1,31 @@
 {
   allowUnfree = true;
   packageOverrides = pkgs: {
-    adcPackages = with pkgs; 
-      let 
+    adcPackages = with pkgs;
+      let
         python-libs = python-libs: with python-libs; [
           debugpy
         ];
         python3WithLibs = python3.withPackages python-libs;
         mariadb = pkgs.mariadb.overrideAttrs (oldAttrs: rec {
-          cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+          cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
             "-DPLUGIN_AUTH_PAM=NO"
           ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
             "-DPLUGIN_AUTH_PAM=NO"
             "-DPLUGIN_AUTH_PAM_V1=NO"
           ];
         });
-      in pkgs.buildEnv {
+      in
+      pkgs.buildEnv {
         name = "adc-packages";
         paths = [
           awscli2
-          (import <nixpkgs-stable> {}).aws-mfa
+          (import <nixpkgs-stable> { }).aws-mfa
           bash
           binutils
           bundix
           curl
+          # ddrescue
           eksctl
           envsubst
           fd
@@ -51,7 +53,6 @@
           mp3val
           neofetch
           nodejs-14_x
-          nodePackages.bash-language-server
           nodePackages.redoc-cli
           pandoc
           python3WithLibs
@@ -65,10 +66,8 @@
           rlwrap
           ruby
           shellcheck
-          shfmt
           # sonar-scanner-cli
-          stylua
-          (import <nixpkgs-stable> {}).terraform_0_12
+          (import <nixpkgs-stable> { }).terraform_0_12
           tmux
           tmux-xpanes
           tree
@@ -76,12 +75,31 @@
           watchman
           wget
           whois
-          (import <nixpkgs-stable> {}).yarn
+          (import <nixpkgs-stable> { }).yarn
           zathura
           zsh
           vault
           envconsul
-          (callPackage (import ../../packages/neovim.nix) {})
+          (callPackage (import ../../packages/neovim.nix) { })
+          # language servers
+          nodePackages.bash-language-server
+          nodePackages.dockerfile-language-server-nodejs
+          nodePackages.pyright
+          nodePackages.typescript-language-server
+          nodePackages.vscode-langservers-extracted
+          nodePackages.vscode-json-languageserver
+          nodePackages.yaml-language-server
+          rnix-lsp
+          sumneko-lua-language-server
+          terraform-ls
+          # linters/diagnostics/formatters
+          nodePackages.eslint
+          python39Packages.flake8
+          pgformatter
+          shfmt
+          stylua
+          black
+          mypy
         ];
         pathsToLink = [ "/share" "/bin" ];
       };
