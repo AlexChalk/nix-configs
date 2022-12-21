4f8c3ac19701e7b74ba46ce3b84bd2965700d066
Alex Chalk <contact@alexchalk.net>
2022-12-21 18:36:53 -0500

    Update to nixos 21.11
    Main change is to how sway is configured, taken from changes to wiki at
    https://nixos.wiki/wiki/Sway

--- t440p/etc/nixos/configuration.nix
+++ t440p/etc/nixos/configuration.nix
@@ -19,6 +19,46 @@ let
     '';
     destination = "/etc/udev/rules.d/50-uhk60.rules";
   };
+
+  # bash script to let dbus know about important env variables and
+  # propagate them to relevent services run at the end of sway config
+  # see
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
+  # some user services to make sure they have the correct environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'Dracula'
+      '';
+  };
 in
 {
   imports =
@@ -135,25 +175,13 @@ in
     pciutils
     pulseaudio
     qemu_kvm
+    sway
+    dbus-sway-environment
+    configure-gtk
     vim
     virtmanager
     wget
     zsh
-    (
-      pkgs.writeTextFile {
-        name = "startsway";
-        destination = "/bin/startsway";
-        executable = true;
-        text = ''
-          #! ${pkgs.bash}/bin/bash
-
-          # first import environment variables from the login manager
-          systemctl --user import-environment
-          # then start the service
-          exec systemctl --user start sway.service
-        '';
-      }
-    )
   ];
 
   environment.variables = {
@@ -278,15 +306,31 @@ in
     };
   };
 
+  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+  # known as portals under a well-known name
+  # (org.freedesktop.portal.Desktop) and object path
+  # (/org/freedesktop/portal/desktop).
+  # The portal interfaces include APIs for file access, opening URIs,
+  # printing and others.
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   # Enable the sway windowing system.
   programs.sway = {
     enable = true;
+    wrapperFeatures.gtk = true;
     extraPackages = with pkgs; [
+      glib
       swaybg
       swaylock # lockscreen
       swayidle
       xdg_utils # copy from screen
-      libappindicator # tray that just works
+      libappindicator-gtk3 # tray that just works
       xwayland # for legacy apps
       waybar # status bar
       bemenu
--- t440p/nixpkgs/config.nix
+++ t440p/nixpkgs/config.nix
@@ -1,135 +1,139 @@
 {
   allowUnfree = true;
   packageOverrides = pkgs: with pkgs; {
-    adcPackages = pkgs.buildEnv {
-      name = "adc-packages";
-      paths = [
-        _1password
-        (import <nixos-unstable> {})._1password-gui
-        acpi
-        appimage-run
-        anki
-        audacity
-        babashka
-        bashmount
-        beets
-        binutils
-        bitwarden
-        bitwarden-cli
-        bundix
-        castget # rss enclosure downloader
-        circleci-cli
-        clj-kondo
-        (import <nixos-unstable> {}).clojure
-        curl
-        dotnet-sdk
-        dotnetPackages.Nuget
-        (import <nixos-unstable> {}).dropbox-cli
-        elmPackages.elm
-        elmPackages.elm-format
-        elmPackages.elm-json
-        elmPackages.elm-test
-        (callPackage (import ../../packages/emoji-launcher.nix) { inherit pkgs; stdenv = pkgs.stdenv; })
-        fd
-        ffmpeg
-        firefox-wayland
-        fsharp
-        fzf
-        (hiPrio gcc)
-        git
-        gitAndTools.gh
-        gitAndTools.hub
-        gnome3.adwaita-icon-theme
-        gnome3.simple-scan
-        gnumake
-        grim
-        heroku
-        hledger
-        htop
-        imagemagick
-        insomnia
-        joker
-        jq
-        keybase-gui
-        kitty
-        leiningen
-        lua5_1
-        maven
-        mono
-        mpv # media player
-        mtools # read ms-dos disks
-        neofetch
-        netlify-cli
-        ngrok
-        nix-index
-        nixopsUnstable
-        nodejs
-        octave
-        jdk11
-        pandoc
-        parted
-        python3
-        rename
-        ripgrep
-        ruby
-        rustup
-        signal-desktop
-        slurp
-        (import <nixos-unstable> {}).sqlfluff
-        (import <nixos-unstable> {}).nodePackages.sql-formatter
-        stack
-        terraform
-        texlive.combined.scheme-full
-        tmux
-        tree
-        universal-ctags
-        unzip
-        viewnior
-        wf-recorder
-        wget
-        whois
-        wine
-        winetricks
-        wl-clipboard
-        xorg.xkbcomp
-        yarn # use yarn instead of npm for global installs
-        zathura
-        zoom-us
-        teams
-        zstd # compression algorithm
-        # Neovim
-        (callPackage (import ../../packages/neovim.nix) { pkgs = import <nixos-unstable> {}; })
-        # Language servers/formatters for neovim
-        clang-tools
-        (import <nixos-unstable> {}).clojure-lsp
-        elmPackages.elm-language-server
-        gopls
-        (import <nixos-unstable> {}).haskell-language-server
-        nodePackages.bash-language-server
-        nodePackages.dockerfile-language-server-nodejs
-        nodePackages.pyright
-        nodePackages.typescript-language-server
-        nodePackages.vscode-langservers-extracted
-        nodePackages.vscode-json-languageserver
-        nodePackages.yaml-language-server
-        rnix-lsp
-        sumneko-lua-language-server
-        terraform-ls
-        rubyPackages.solargraph
-        rust-analyzer
-        shellcheck
-        (import <nixos-unstable> {}).stylua
-        texlab
-        # linters/diagnostics/formatters
-        nodePackages.eslint
-        python39Packages.flake8
-        pgformatter
-        shfmt
-        black
-        mypy
-        shfmt
-      ];
-      pathsToLink = [ "/share" "/bin" ];
-    };
+    adcPackages =
+      let
+        nixops' = (import <nixos-unstable> { }).nixopsUnstable;
+      in
+      pkgs.buildEnv {
+        name = "adc-packages";
+        paths = [
+          _1password
+          (import <nixos-unstable> { })._1password-gui
+          acpi
+          appimage-run
+          anki
+          audacity
+          babashka
+          bashmount
+          beets
+          binutils
+          bitwarden
+          bitwarden-cli
+          bundix
+          castget # rss enclosure downloader
+          circleci-cli
+          clj-kondo
+          (import <nixos-unstable> { }).clojure
+          curl
+          dotnet-sdk
+          dotnetPackages.Nuget
+          (import <nixos-unstable> { }).dropbox-cli
+          elmPackages.elm
+          elmPackages.elm-format
+          elmPackages.elm-json
+          elmPackages.elm-test
+          (callPackage (import ../../packages/emoji-launcher.nix) { inherit pkgs; stdenv = pkgs.stdenv; })
+          fd
+          ffmpeg
+          firefox-wayland
+          fsharp
+          fzf
+          (hiPrio gcc)
+          git
+          gitAndTools.gh
+          gitAndTools.hub
+          gnome3.adwaita-icon-theme
+          gnome3.simple-scan
+          gnumake
+          grim
+          heroku
+          hledger
+          htop
+          imagemagick
+          insomnia
+          joker
+          jq
+          keybase-gui
+          kitty
+          leiningen
+          lua5_1
+          maven
+          mono
+          mpv # media player
+          mtools # read ms-dos disks
+          neofetch
+          netlify-cli
+          ngrok
+          nix-index
+          # nixops'
+          nodejs
+          octave
+          jdk11
+          pandoc
+          parted
+          python3
+          rename
+          ripgrep
+          ruby
+          rustup
+          signal-desktop
+          slurp
+          (import <nixos-unstable> { }).sqlfluff
+          (import <nixos-unstable> { }).nodePackages.sql-formatter
+          stack
+          terraform
+          texlive.combined.scheme-full
+          tmux
+          tree
+          universal-ctags
+          unzip
+          viewnior
+          wf-recorder
+          wget
+          whois
+          wine
+          winetricks
+          wl-clipboard
+          xorg.xkbcomp
+          yarn # use yarn instead of npm for global installs
+          zathura
+          zoom-us
+          teams
+          zstd # compression algorithm
+          # Neovim
+          (callPackage (import ../../packages/neovim.nix) { pkgs = import <nixos-unstable> { }; })
+          # Language servers/formatters for neovim
+          clang-tools
+          (import <nixos-unstable> { }).clojure-lsp
+          elmPackages.elm-language-server
+          gopls
+          (import <nixos-unstable> { }).haskell-language-server
+          nodePackages.bash-language-server
+          nodePackages.dockerfile-language-server-nodejs
+          nodePackages.pyright
+          nodePackages.typescript-language-server
+          nodePackages.vscode-langservers-extracted
+          nodePackages.vscode-json-languageserver
+          nodePackages.yaml-language-server
+          rnix-lsp
+          sumneko-lua-language-server
+          terraform-ls
+          rubyPackages.solargraph
+          rust-analyzer
+          shellcheck
+          (import <nixos-unstable> { }).stylua
+          texlab
+          # linters/diagnostics/formatters
+          nodePackages.eslint
+          python3Packages.flake8
+          pgformatter
+          shfmt
+          black
+          mypy
+          shfmt
+        ];
+        pathsToLink = [ "/share" "/bin" ];
+      };
   };
 }
