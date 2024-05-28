c3ce6b4609e05a179d8ecd2dd2f8b425e2497c8c
Alex Chalk <contact@alexchalk.net>
2024-05-28 11:47:25 -0400

    Deflake configuration.nix on t480

--- t480/etc/nixos/configuration.nix
+++ t480/etc/nixos/configuration.nix
@@ -2,19 +2,30 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, nixos-hardware, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      unstable = import <nixos-unstable> {
+        config = config.nixpkgs.config;
+      };
+
+      stable = import <nixos-stable> {
+        config = config.nixpkgs.config;
+      };
+    })
+  ];
 
   imports =
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      nixos-hardware.nixosModules.lenovo-thinkpad-t480
+      <nixos-hardware/lenovo/thinkpad/t480>
       ../../../shared/default-nixos-linux.nix
     ];
 
